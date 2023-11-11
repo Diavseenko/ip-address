c32f452a979b2a3ec282f1f3ec7fe2cb557d2d1d
DmitrY <diavseenko@yandex.ru>
2023-11-11 15:47:13 +0000

    swtich on func real IP

--- src/components/Header/SearchBlock.js
+++ src/components/Header/SearchBlock.js
@@ -4,16 +4,16 @@ import { useMap, useMapEvents } from 'react-leaflet/hooks';
 
 export default function SearchBlock({ result, setResult }) {
 
-   const baseObj = {
-      ipAddress: "192.212.174.101",
-      timeZone: "-07:00",
-      locationsCity: "South San Gabriel",
-      locationsRegion: "California",
-      locationsPostalCode: "123456",
-      isp: "Southern California Edison",
-      lat: 34.04915,
-      lng: -118.09462,
-   };
+   // const baseObj = {
+   //    ipAddress: "192.212.174.101",
+   //    timeZone: "-07:00",
+   //    locationsCity: "South San Gabriel",
+   //    locationsRegion: "California",
+   //    locationsPostalCode: "123456",
+   //    isp: "Southern California Edison",
+   //    lat: 34.04915,
+   //    lng: -118.09462,
+   // };
 
    const [input, setInput] = useState("");
    var [resultSB, setResultSB] = useState({});
@@ -30,63 +30,28 @@ export default function SearchBlock({ result, setResult }) {
          lat: resultSB.lat,
          lng: resultSB.lng,
       });
-      let infoSB = Object.assign(resultSB);
-      // let result = [];
-      // infoSB.map((inf) => {
-      //    if (inf[0] === 'ipAddress') {
-      //       result.push(inf)
-      //    } else if (inf[0] === 'ipAddress') {
-      //       result.push(inf)
-      //    } else if (inf[0] === 'ipAddress') {
-      //       result.push(inf)
-      //    } else if (inf[0] === 'ipAddress') {
-      //       result.push(inf)
-      //    }
-
-      // })
-      // setInfo(info.map((inf) => {
-      //    inf[0] = []push(infoSB[0])
-      // }))
    }, [resultSB]);
-
 
    const onSubmit = (e) => {
       e.preventDefault();
-      // var ip = input.toString();
+      var ip = input.toString();
 
-      // axios.get('https://geo.ipify.org/api/v2/country,city?apiKey=at_4KUoFiBFxHG1eXz6wa6sOqt9oFplt&ipAddress=' + ip).then(
-      //    res => {
-      //       console.log(res);
-      //       resultSB.ipAddress = res.data.ip;
-      //       resultSB.timeZone = res.data.location.timezone;
-      //       resultSB.locations = res.data.location.city + res.data.location.region + res.data.location.postalCode;
-      //       resultSB.isp = res.data.isp;
-      //       resultSB.lat = res.data.location.lat;
-      //       resultSB.lng = res.data.location.lng;
-      //       cc
-      //    });
-      // ChangeView([baseObj.lat, baseObj.lng], 13);
+      axios.get('https://geo.ipify.org/api/v2/country,city?apiKey=at_4KUoFiBFxHG1eXz6wa6sOqt9oFplt&ipAddress=' + ip).then(
+         res => {
+            console.log(res);
+            setResultSB({
+               ...resultSB,
+               ipAddress: res.data.ip,
+               timeZone: res.data.location.timezone,
+               locationsCity: res.data.location.city,
+               locationsRegion: res.data.location.region,
+               locationsPostalCode: res.data.location.postalCode,
+               isp: res.data.isp,
+               lat: res.data.location.lat,
+               lng: res.data.location.lng,
+            })
+         });
 
-      return setResultSB({
-         ...resultSB,
-         ipAddress: baseObj.ipAddress,
-         timeZone: baseObj.timeZone,
-         locationsCity: baseObj.locationsCity,
-         locationsRegion: baseObj.locationsRegion,
-         locationsPostalCode: baseObj.locationsPostalCode,
-         isp: baseObj.isp,
-         lat: baseObj.lat,
-         lng: baseObj.lng,
-      });
-
-      // resultSB.ipAddress = baseObj.ipAddress;
-      // resultSB.timeZone = baseObj.timeZone;
-      // resultSB.locationsCity = baseObj.locationsCity;
-      // resultSB.locationsRegion = baseObj.locationsRegion;
-      // resultSB.locationsPostalCode = baseObj.locationsPostalCode;
-      // resultSB.isp = baseObj.isp;
-      // resultSB.lat = baseObj.lat;
-      // resultSB.lng = baseObj.lng;
    };
    return (
       <form className='search' onSubmit={(e) => onSubmit(e)}>
@@ -94,7 +59,6 @@ export default function SearchBlock({ result, setResult }) {
             <input className='search-input' type='text' placeholder='IP XXX.XXX.XXX.XXX' value={input} onChange={(e) => setInput(e.target.value)} />
             <input className='search-submit' type='submit' value="&gt;" />
          </div>
-
       </form>
    )
 };
